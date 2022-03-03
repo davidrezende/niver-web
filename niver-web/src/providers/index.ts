0190bfb361d5a6668f23894d79f61c7776190485
davidrezende <davidpenalives@gmail.com>
2022-03-03 19:59:20 -0300

    correcao de data ao alterar no perfil

--- niver-web/src/providers/index.ts
+++ niver-web/src/providers/index.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-// export const BaseApi = axios.create({ baseURL: 'http://localhost:8090' });
-// export const PersonApi = axios.create({ baseURL: 'http://localhost:8090/person/api' });
-// export const GroupApi = axios.create({ baseURL: 'http://localhost:8090/group/api' });
-// export const MemberApi = axios.create({ baseURL: 'http://localhost:8090/member/api' });
-// export const CalendarApi = axios.create({ baseURL: 'http://localhost:8090/calendar/api' });
-// export const InviteApi = axios.create({ baseURL: 'http://localhost:8090/invite/api' });
+export const BaseApi = axios.create({ baseURL: 'http://localhost:8090' });
+export const PersonApi = axios.create({ baseURL: 'http://localhost:8090/person/api' });
+export const GroupApi = axios.create({ baseURL: 'http://localhost:8090/group/api' });
+export const MemberApi = axios.create({ baseURL: 'http://localhost:8090/member/api' });
+export const CalendarApi = axios.create({ baseURL: 'http://localhost:8090/calendar/api' });
+export const InviteApi = axios.create({ baseURL: 'http://localhost:8090/invite/api' });
 
-export const BaseApi = axios.create({ baseURL: 'http://niverdequem.tk:8090' });
-export const PersonApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/person/api' });
-export const GroupApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/group/api' });
-export const MemberApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/member/api' });
-export const CalendarApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/calendar/api' });
-export const InviteApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/invite/api' });
+// export const BaseApi = axios.create({ baseURL: 'http://niverdequem.tk:8090' });
+// export const PersonApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/person/api' });
+// export const GroupApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/group/api' });
+// export const MemberApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/member/api' });
+// export const CalendarApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/calendar/api' });
+// export const InviteApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/invite/api' });
 
 // PersonApi.interceptors.request.use(function (config) {
 //   const token = localStorage.getItem('@App:token');

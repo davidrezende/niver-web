28ce254f9f83ebdb2368726a3fad662104c93000
David Rezende <61335298+davidrezende@users.noreply.github.com>
2022-03-06 20:59:39 -0300

    Test (#49)
    * nginx
    
    * ajustando script
    
    * adding new commands in dockercompose
    
    * testando docker-compose com react
    
    * teste
    
    * test deploy
    
    * retirando redirect proxy
    
    * alterando path da api para https
    
    * build folder
    
    * alterando port
    
    * add key api
    
    * ajustando worflow
    
    * deletando arquivos
    
    * removendo arquivos build

--- niver-web/src/providers/index.ts
+++ niver-web/src/providers/index.ts
@@ -7,12 +7,12 @@ import axios from 'axios';
 // export const CalendarApi = axios.create({ baseURL: 'http://localhost:8090/calendar/api' });
 // export const InviteApi = axios.create({ baseURL: 'http://localhost:8090/invite/api' });
 
-export const BaseApi = axios.create({ baseURL: 'http://niverdequem.tk:8090' });
-export const PersonApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/person/api' });
-export const GroupApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/group/api' });
-export const MemberApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/member/api' });
-export const CalendarApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/calendar/api' });
-export const InviteApi = axios.create({ baseURL: 'http://niverdequem.tk:8090/invite/api' });
+export const BaseApi = axios.create({ baseURL: 'https://niverdequem.tk:8443' });
+export const PersonApi = axios.create({ baseURL: 'https://niverdequem.tk:8443/person/api' });
+export const GroupApi = axios.create({ baseURL: 'https://niverdequem.tk:8443/group/api' });
+export const MemberApi = axios.create({ baseURL: 'https://niverdequem.tk:8443/member/api' });
+export const CalendarApi = axios.create({ baseURL: 'https://niverdequem.tk:8443/calendar/api' });
+export const InviteApi = axios.create({ baseURL: 'https://niverdequem.tk:8443/invite/api' });
 
 // PersonApi.interceptors.request.use(function (config) {
 //   const token = localStorage.getItem('@App:token');
